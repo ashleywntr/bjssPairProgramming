06f055f1bef95e6607a249f1ab0fdc6bf015f095
Ashley Winter <ashleywntr@gmail.com>
2022-08-12 20:39:16 +0100

    working with tests

--- main.py
+++ main.py
@@ -4,26 +4,30 @@ def calculate_lbtt(value):
     lbtt_cost = 0
     if 0 < value < 145000:
         lbtt_cost = 0
-    elif 145000 < value < 250000:
+    elif 145000 < value <= 250000:
         lbtt_cost = value * 0.02
-    elif 250000 < value < 325000:
+    elif 250000 < value <= 325000:
         lbtt_cost = value * 0.05
-    elif 325000 < value < 750000:
+    elif 325000 < value <= 750000:
         lbtt_cost = value * 0.1
     elif 750000 < value:
         lbtt_cost = value * 0.12
     else: raise ValueError
     return round(lbtt_cost, 2)
+
+
 def interface():
     try:
-        price_of_property = round(float(input("\nPlease enter property value to calculate LBTT: ")), 2)
+        price_of_property = round(float(input("\nPlease enter property value to calculate LBTT: £")), 2)
         lbtt = calculate_lbtt(price_of_property)
         print(f"The price of the property is £{price_of_property}")
         print(f"The value of LBTT to be paid is £{lbtt}")
         interface()
-    except Exception:
-        print("Please enter a valid input.")
+    except ValueError:
+        print("Please enter an input greater than 0.")
         interface()
+    except TypeError:
+        print("Please enter a numerical input.")
 
 
 if __name__ == "__main__":
---
+++ tests.py
@@ -0,0 +1,25 @@
+import unittest
+from unittest import TestCase
+from main import calculate_lbtt
+
+
+class InitialTest(TestCase):
+
+    def test_0_percent_value(self):
+        self.assertEqual(0, calculate_lbtt(500))
+
+    def test_negative_value(self):
+        self.assertRaises(ValueError, calculate_lbtt, -1)
+
+    def test_string_value(self):
+        self.assertRaises(TypeError, calculate_lbtt, "Not a float")
+
+    def test_12_percent_value(self):
+        self.assertAlmostEqual(97440, calculate_lbtt(812000))
+
+    def test_5_percent_value(self):
+        self.assertAlmostEqual(16250, calculate_lbtt(325000))
+
+
+if __name__ == "__tests__":
+    unittest.main()
